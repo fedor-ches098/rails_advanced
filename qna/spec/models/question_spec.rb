9757cc0a49ab8db39cb6a7169c7723e51bc20488
fedor-ches098 <fedor-ches098>
2024-07-21 17:52:41 +0300

    edit lesson_4

--- qna/spec/models/question_spec.rb
+++ qna/spec/models/question_spec.rb
@@ -2,6 +2,7 @@ require 'rails_helper'
 
 RSpec.describe Question, type: :model do
   it {should belong_to(:test)}
+  it {should have_many(:answers).dependent(:destroy)}
 
   it {should validate_presence_of :body }
 end
--- qna/spec/models/test_spec.rb
+++ qna/spec/models/test_spec.rb
@@ -1,7 +1,7 @@
 require 'rails_helper'
 
 RSpec.describe Test, type: :model do
-  it {should have_many(:questions)}
+  it {should have_many(:questions).dependent(:destroy)}
 
   it {should validate_presence_of :title }
   it {should validate_numericality_of(:level).only_integer.is_greater_than_or_equal_to(0) }
