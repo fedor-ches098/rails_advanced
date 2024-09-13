77cf47f93fea8e49d355f754f1dd71e2079158cc
fedor-ches098 <fedor-ches098>
2024-09-13 11:53:35 +0300

    edit lesson_10

--- qna/spec/support/concerns/likable_spec.rb
+++ qna/spec/support/concerns/likable_spec.rb
@@ -6,12 +6,7 @@ RSpec.shared_examples_for 'likable' do
   let(:likers) { create_list(:user, 5) }
 
   let(:likable) do
-    if model.to_s == 'Answer'
-      question = create(:question, user: author)
-      create(model.to_s.underscore.to_sym, question: question, user: author)
-    else
-      create(model.to_s.underscore.to_sym, user: author)
-    end
+    liked(model, author)
   end
 
   describe '#vote_up' do
