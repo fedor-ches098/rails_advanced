e48085f1d0800e651bda62774744c65a6848cf82
fedor-ches098 <fedor-ches098>
2024-10-06 19:24:27 +0300

    add mail smtp

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -97,6 +97,17 @@ Rails.application.configure do
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
 
+  config.action_mailer.default_url_options = { :host => "89.169.145.210" }
+  config.action_mailer.delivery_method = :smtp
+  config.action_mailer.smtp_settings = {
+    address:               'smtp.gmail.com',
+    port:                 587,
+    user_name:            'fedorchesnokov98@gmail.com',
+    password:             'qbbhebedjjrjcyuh',
+    authentication:       'plain',
+    enable_starttls_auto: true
+  }
+
   # Inserts middleware to perform automatic connection switching.
   # The `database_selector` hash is used to pass options to the DatabaseSelector
   # middleware. The `delay` is used to determine how long to wait after a write
