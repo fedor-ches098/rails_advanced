32b42396c0f42fb81f88d2a6d0360f3eb6471620
fedor-ches098 <fedor-ches098>
2024-10-07 10:04:58 +0300

    add unicorn

--- Gemfile
+++ Gemfile
@@ -35,6 +35,7 @@ gem 'thinking-sphinx'
 gem 'mysql2'
 gem 'database_cleaner'
 gem 'redis', '~> 4.0'
+gem 'unicorn'
 
 group :development, :test do
   # Call 'byebug' anywhere in the code to stop execution and get a debugger console
--- config/environments/production.rb
+++ config/environments/production.rb
@@ -102,8 +102,8 @@ Rails.application.configure do
   config.action_mailer.smtp_settings = {
     address:               'smtp.gmail.com',
     port:                 587,
-    user_name:            'fedorchesnokov98@gmail.com',
-    password:             'qbbhebedjjrjcyuh',
+    user_name:            ENV['SMTP_USERNAME'],
+    password:             ENV['SMTP_PASSWORD'],
     authentication:       'plain',
     enable_starttls_auto: true
   }
