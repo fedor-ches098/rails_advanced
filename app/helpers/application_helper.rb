0b35e1b4bcaeeb47cabdc606056aa8131b429a6f
fedor-ches098 <fedor-ches098>
2024-10-09 12:37:44 +0300

    add cache

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,13 @@
 module ApplicationHelper
+  def link_cache_key(link)
+    link_type = link.gist? ? 'gist' : 'simple'
+
+    "links/#{link.id}/#{link_type}-#{link.updated_at.to_f}"
+  end
+
+  def answer_cache_key(answer)
+    answer_type = answer.best? ? 'best' : 'simple'
+
+    "answers/#{answer.id}/#{answer_type}-#{answer.updated_at.to_f}"
+  end
 end
